aaba7fe9618884f51a0f3ce9d534cf5a25ab19f7
kieserver <>
2018-11-01 00:46:28 +0530

    tttttttttttt {/src/main/java/com/neo/drools/HelloWorldExample.java}

--- src/main/java/com/neo/drools/HelloWorldExample.java
+++ src/main/java/com/neo/drools/HelloWorldExample.java
@@ -35,6 +35,7 @@ public class HelloWorldExample {
         KieServices ks = KieServices.Factory.get();
         System.err.println("done");
          System.err.println("done");
+         System.err.println("done");
         // From the kie services, a container is created from the classpath
         KieContainer kc = ks.getKieClasspathContainer();
 	
